b2afecb1df95d317a515d2065c5e40ae24a0b762
gox <gox.roche@gmail.com>
2023-01-12 18:46:56 +0100

    Add port as program parameter

--- pg-broker-server/src/main/java/org/gox/pg/broker/PgBrokerMain.java
+++ pg-broker-server/src/main/java/org/gox/pg/broker/PgBrokerMain.java
@@ -1,5 +1,7 @@
 package org.gox.pg.broker;
 
+import org.apache.commons.lang3.StringUtils;
+import org.gox.pg.broker.exception.PgBrokerRuntimeException;
 import org.gox.pg.broker.server.PgBroker;
 
 import java.io.IOException;
@@ -7,6 +9,9 @@ import java.io.IOException;
 public class PgBrokerMain {
 
     public static void main(String[] args) throws IOException {
-        new PgBroker(1337).start();
+        if(args.length == 0 || StringUtils.isEmpty(args[0]) || !StringUtils.isNumeric(args[0])) {
+            throw new PgBrokerRuntimeException("Incorrect port number parameter");
+        }
+        new PgBroker(Integer.parseInt(args[0])).start();
     }
 }
